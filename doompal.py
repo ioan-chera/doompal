87815266e682fccb4db797bdfbffdb5f72241c02
Ioan Chera <ioan.chera@gmail.com>
2020-09-19 18:22:54 +0300

    Added options and ignored tmp

--- doompal.py
+++ doompal.py
@@ -40,6 +40,12 @@ def inverse_luminance(color):
     return yuv_to_rgb(yuv_inverse)
 
 
+def toggled_chrominance(color):
+    yuv = rgb_to_yuv(color)
+    yuv_toggled = (yuv[0], yuv[2], yuv[1])
+    return yuv_to_rgb(yuv_toggled)
+
+
 def distance2(color1, color2):
     return (color1[0] - color2[0]) ** 2 + (color1[1] - color2[1]) ** 2 + (color1[2] - color2[2]) ** 2
 
@@ -59,6 +65,9 @@ def run_doompal():
     palette_path = sys.argv[1]
     colormap_path = sys.argv[2]
     dest_path = sys.argv[3]
+    command = None
+    if len(sys.argv) > 4:
+        command = sys.argv[4]
 
     with open(palette_path, 'rb') as f:
         palette_raw = f.read()
@@ -70,7 +79,12 @@ def run_doompal():
 
     palette = get_colors(palette_raw)
     for index, color in enumerate(palette):
-        inv_color = inverse_luminance(color)
+        if command == 'toggle':
+            inv_color = toggled_chrominance(color)
+        elif command == 'inverse_toggle':
+            inv_color = inverse_luminance(toggled_chrominance(color))
+        else:
+            inv_color = inverse_luminance(color)
         inv_pos = closest(inv_color, palette)
         colormap_out[32 * 256 + index] = inv_pos
 
